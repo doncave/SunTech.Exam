36091198fbd3b53d88346637c4da56bdc1eb5727
Don <don@delegia.se>
2023-09-24 16:18:04 +0800

    Added CosmosDb nuget; Added Person class

---
+++ SunTech.Exam/Model/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace SunTech.Exam.Model
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int BirthdayInEpoch { get; set; }
+
+        public string Email { get; set; }
+    }
+}
