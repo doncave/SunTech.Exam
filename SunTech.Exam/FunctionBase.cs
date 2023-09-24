f7fea51776e2c5e155366f49da3df4c32c28202b
Don <don@delegia.se>
2023-09-24 23:43:46 +0800

    Created FunctionBase to provide a generic database initialization for both Write and Read operations (CQRS design)

---
+++ SunTech.Exam/FunctionBase.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Azure.Cosmos;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System;
+
+namespace SunTech.Exam
+{
+    public class FunctionBase
+    {
+        // The Cosmos client instance
+        protected CosmosClient CosmosClient;
+        protected Database Database;
+        protected Microsoft.Azure.Cosmos.Container Container;
+
+        // The name of the database and container we will create
+        private string databaseId = "Town";
+        private string containerId = "Person";
+
+        public FunctionBase() 
+        {
+        }
+
+        /// <summary>
+        /// Initialize database 
+        /// </summary>
+        /// <returns></returns>
+        protected async Task InitDatabase()
+        {
+            await CreateDatabaseAsync();
+            await CreateContainerAsync();
+        }
+
+        #region Private Methods
+        
+        // <CreateDatabaseAsync>
+        /// <summary>
+        /// Create the database if it does not exist
+        /// </summary>
+        private async Task CreateDatabaseAsync()
+        {
+            Database = await CosmosClient.CreateDatabaseIfNotExistsAsync(databaseId);
+            Console.WriteLine("Created Database: {0}\n", Database.Id);
+        }
+
+        // <CreateContainerAsync>
+        /// <summary>
+        /// Create the container if it does not exist. 
+        /// Specifiy "/partitionKey" as the partition key path since we're storing family information, to ensure good distribution of requests and storage.
+        /// </summary>
+        /// <returns></returns>
+        private async Task CreateContainerAsync()
+        {
+            Container = await Database.CreateContainerIfNotExistsAsync(containerId, "/partitionKey");
+            Console.WriteLine("Created Container: {0}\n", Container.Id);
+        }
+
+        #endregion
+    }
+}
